ed430df512b03486486810bcabc53958f17ebd07
Anthony Sterling <mail@anthonysterling.com>
2013-12-26 21:35:50 +0000

    Added service to serve Jekyll at provision time

--- _env/provision.sh
+++ _env/provision.sh
@@ -1,5 +1,6 @@
 echo 'if [ -d /vagrant ]; then cd /vagrant; fi' >> /home/vagrant/.bashrc
-apt-get -y update
+apt-get -qq -y update
 apt-get -y install ruby1.9.3 build-essential
 gem install jekyll -v "=1.4.2" --no-rdoc --no-ri
-jekyll serve --detach --source /vagrant
+cp -f /vagrant/_env/jekyll-serve.conf /etc/init/
+service jekyll-serve start
