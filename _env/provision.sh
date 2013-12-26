36030c7385371dc218a27bd107234e30da01604a
Anthony Sterling <mail@anthonysterling.com>
2013-12-26 21:59:28 +0000

    Update provision.sh

--- _env/provision.sh
+++ _env/provision.sh
@@ -2,5 +2,5 @@ echo 'if [ -d /vagrant ]; then cd /vagrant; fi' >> /home/vagrant/.bashrc
 apt-get -qq -y update
 apt-get -y install ruby1.9.3 build-essential
 gem install jekyll -v "=1.4.2" --no-rdoc --no-ri
-cp -f /vagrant/_env/jekyll-serve.conf /etc/init/
+cp -f /vagrant/_env/jekyll-server.conf /etc/init/
 service jekyll-server start
