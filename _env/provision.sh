7efa29146d667b9abe8108185a96a36192979026
Anthony Sterling <mail@anthonysterling.com>
2013-12-26 21:52:21 +0000

    Renamed jekyll-serve to jekyll-server

--- _env/provision.sh
+++ _env/provision.sh
@@ -3,4 +3,4 @@ apt-get -qq -y update
 apt-get -y install ruby1.9.3 build-essential
 gem install jekyll -v "=1.4.2" --no-rdoc --no-ri
 cp -f /vagrant/_env/jekyll-serve.conf /etc/init/
-service jekyll-serve start
+service jekyll-server start
