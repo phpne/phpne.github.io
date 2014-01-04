0bb1143d7b2097ae39052f96125c17ba11e8b646
Martin Bean <martin@martinbean.co.uk>
2014-01-04 19:19:34 +0000

    Added event details to footer

--- js/main.js
+++ js/main.js
@@ -4,4 +4,27 @@
         e.preventDefault();
         window.open(this.href);
     });
+    
+    // initialize Google Map
+    var claveringHouse = new google.maps.LatLng(54.9682764, -1.6122045);
+    var map = new google.maps.Map(document.getElementById('location-map'), {
+        center: claveringHouse,
+        disableDefaultUI: true,
+        zoom: 18
+    });
+    var marker = new google.maps.Marker({
+        map: map,
+        position: claveringHouse,
+        title: 'Clavering House'
+    });
+    var infowindow = new google.maps.InfoWindow({
+        content: '<strong>Clavering House</strong><br />' +
+                 'Clavering Place<br />' +
+                 'Newcastle upon Tyne<br />' +
+                 'NE1 3NG'
+    });
+    
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+    });
 })(jQuery);
